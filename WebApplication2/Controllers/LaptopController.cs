3c01949be04dcba516f57ef935d5b60da4ef5a90
Administrator <Administrator@PC704>
2020-10-29 16:26:35 +0700

    Add project files.

---
+++ WebApplication2/Controllers/LaptopController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class LaptopController : Controller
+    {
+        TMDT5Entities db = new TMDT5Entities();
+        // GET: Laptop 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getCompany()
+        {
+            var ListCompany = db.laptops.GroupBy(l => l.Company).Select(l => l.Key);
+            return PartialView(ListCompany);
+        }
+        public ActionResult getCPU()
+        {
+            var ListCPU = db.laptops.GroupBy(l => l.Cpu).Select(l => l.Key);
+            return PartialView(ListCPU);
+        }
+        public ActionResult getScreenResolution()
+        {
+            var ListScreenResolution = db.laptops.GroupBy(l => l.ScreenResolution).Select(l => l.Key);
+            return PartialView(ListScreenResolution);
+        }
+        public ActionResult getTypeName()
+        {
+            var ListTypeName = db.laptops.GroupBy(l => l.TypeName).Select(l => l.Key);
+            return PartialView(ListTypeName);
+        }
+        /// Sv tự thêm các thông tin khác 
+    }
+}
---
+++ WebApplication2/Helper/SortCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace WebApplication2.Helper
+{
+    public enum SortCriteria
+    {
+        [Description("Relevance")]
+        Relevance = 0, 
+        [Description("Price: Low to High")]
+        PriceLowToHigh = 1, 
+        [Description("Price: High to Low")]
+        PriceHighToLow = 2 
+    }
+}
